82fadcd7b84f6e8dccb80e79464921ea9eab0e42
John <john@example.com>
2026-01-01 19:26:23 +0500

    fix mini

--- app/routes/tests.py
+++ app/routes/tests.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, Depends, HTTPException, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 from typing import Optional
 
 from app.database import get_db
